5d9cb4e8d4decb44992571204feedc99173a63bc
Ivan <vaniashatuhomail@mail.ru>
2019-03-24 01:50:17 +0300

    Added new model

--- oreado_backend/celery.py
+++ oreado_backend/celery.py
@@ -5,13 +5,19 @@ import pickle
 
 import google.oauth2.credentials
 
+from django.contrib.auth import get_user_model
+
 from bs4 import BeautifulSoup
 from celery import Celery
 
 from auth_page.models import Credential
+from mails.models import MailSender
 from box.gmail.models import Gmail
 from mails.models import Mail
 from clean import clean_text_main, rec_tag, delete_extra_text
+
+
+User = get_user_model()
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oreado_backend.settings")
@@ -30,6 +36,9 @@ app.autodiscover_tasks()
 
 MODEL_FILENAME = 'finalized_model3.sav'
 loaded_model = pickle.load(open(MODEL_FILENAME, 'rb'))
+
+NEWS_DIGEST_ID = 3
+INFO_MESSAGE_ID = 3
 
 
 @app.task(bind=True)
@@ -75,6 +84,55 @@ def classify_mail_category():
     for mail, prediction in zip(mails, predictions):
         mail.category_id = prediction
         mail.save()
+
+
+@app.task
+def mail_sender_active():
+    DATA = {}
+
+    all_users = User.objects.all()
+    all_mails = Mail.objects.all()
+
+    mails_to_classify = []
+
+    for mail in all_mails:
+        if mail.category is None:
+            mails_to_classify.append(mail)
+
+    html_bodies = []
+
+    tags = {i: [] for i in range(len(mails_to_classify))}
+
+    for ind, mail in enumerate(mails_to_classify):
+        html = '<div>' + clean_text_main(mail.html_body) + '</div>'
+        rec_tag(ind, BeautifulSoup(html, 'html.parser'), tags)
+
+        html = delete_extra_text(tags[ind])
+        html_bodies.append(' '.join(html))
+
+    predictions = loaded_model.predict(html_bodies)
+
+    for mail, prediction in zip(mails_to_classify, predictions):
+        mail.category_id = prediction
+        mail.save()
+
+    for user in all_users:
+        for mail in all_mails:
+            if f"{user.id}-{mail.come_from}" in DATA:
+                DATA[f"{user.id}-{mail.come_from}"].append(mail.category_id)
+            else:
+                DATA[f"{user.id}-{mail.come_from}"] = []
+
+    for key, value in DATA.items():
+        digest_percent = value.count(NEWS_DIGEST_ID) / len(value)
+        info_percent = value.count(INFO_MESSAGE_ID) / len(value)
+
+        average = (digest_percent + info_percent) / 2
+
+        MailSender.objects.get_or_create(
+            name=key.split('-')[1], user_id=key.split('-')[0],
+            defaults={'is_active': True if average >= 0.5 else False}
+        )
 
 
 if __name__ == "__main__":
