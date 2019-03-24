23b74b26ef99f93c0a25e8855987735a02cfe380
Ivan <vaniashatuhomail@mail.ru>
2019-03-25 00:32:18 +0300

    Fixed int bug

--- box/gmail/models.py
+++ box/gmail/models.py
@@ -231,7 +231,9 @@ class Gmail:
                     res["go_to_email"] = scrap_mail_from_text(d["value"])
             else:
                 count += 1
-                res["owner_id"] = self.owner
+                res["owner_id"] = (self.owner if isinstance(self.owner, int)
+                                   else self.owner.id)
+
                 Mail.objects.create(**res)
                 self.messages.append(message)
                 self.html_messages.append(text_body)
