56b7dc281b241942c2830cfccabe61f38c01d255
Ivan <vaniashatuhomail@mail.ru>
2019-03-24 18:41:09 +0300

    Added mail list, detail

--- mails/api/views.py
+++ mails/api/views.py
@@ -10,14 +10,13 @@ from rest_framework.authentication import (
 from rest_framework.views import APIView
 from django.utils import timezone
 
-from mails.api.serializers import MailListSerializer, MailDetailSerializer
+from mails.api.serializers import MailDetailSerializer
 from mails.models import Mail
 
 from mails.models import MailSender
 
 
 class MailListAPIView(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
@@ -28,12 +27,25 @@ class MailListAPIView(APIView):
             category_id__in=[3, 2]
         ).values('id', 'cleaned_date', 'come_from', 'snippet', 'text_body')
 
+        data_to_send = []
+
+        for mail in mails:
+            data_to_send.append({
+                'id': mail['id'],
+                'cleaned_date': mail['cleaned_date'],
+                'come_from': mail['come_from'],
+                'snippet': mail['snippet'],
+                'text_body': mail['text_body'][:150],
+            })
         return Response({'data': mails})
 
 
-class MailDetailAPIView(RetrieveAPIView):
-    queryset = Mail.objects.all()
-    serializer_class = MailDetailSerializer
+class MailDetailAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, pk):
+        mail = Mail.objects.get(id=pk)
+        return Response({'data': MailDetailSerializer(mail).data})
 
 
 class SendersListAPIView(APIView):
