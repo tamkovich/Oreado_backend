e4102ab460d52b97f1f3c801be7152e9bdc98e3b
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-03-30 23:53:00 +0300

    validate_credentials before any requests

--- oreado_backend/celery.py
+++ oreado_backend/celery.py
@@ -57,7 +57,10 @@ def load_mails():
             owner=cred,
             logger=loggers.get_logger('load_mails')
         )
-        mail.list_messages_one_step("me", count_messages=100)
+        if mail.validate_credentials():
+            mail.list_messages_one_step("me", count_messages=100)
+        else:
+            mail.is_active = False
 
 
 @app.task
@@ -68,9 +71,11 @@ def load_mails_for_user(credentials_data, cred_id, user_id):
         owner=cred_id,
         logger=loggers.get_logger('load_mails_for_user')
     )
-
-    mail.list_messages_one_step("me", count_messages=100)
-    mail_sender_active_by_user_id.delay(user_id)
+    if mail.validate_credentials():
+        mail.list_messages_one_step("me", count_messages=100)
+        mail_sender_active_by_user_id.delay(user_id)
+    else:
+        mail.is_active = False
 
 
 @app.task
