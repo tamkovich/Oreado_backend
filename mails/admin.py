c9da1da2cc85ec86bc786ecc8d5f8cdb370dc5e1
jaselnik <jaselnikpromise@gmail.com>
2019-03-30 19:49:16 +0000

    new admin field

--- mails/admin.py
+++ mails/admin.py
@@ -4,7 +4,7 @@ from mails.models import Mail, MailCategory, MailSender
 
 
 class MailAdmin(admin.ModelAdmin):
-    search_fields = ('come_from', 'go_to', 'text_body')
+    search_fields = ('message_id', 'come_from', 'go_to', 'text_body')
 
 
 admin.site.register(Mail, MailAdmin)
